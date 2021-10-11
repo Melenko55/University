0ec22973f8277a3c03a251d0f1b403d38003eb52
Melya <melenko55@gmail.com>
2021-10-11 16:24:59 +0300

    Started second lab

---
+++ Term1/Algoritms/Lab1/InverseMatrixLU/InverseMatrixLU/InverseMatrixLU.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+void inverseByLU(vector<vector<double>>, int, int);
+void inverseMatrix(vector<vector<double>>, int, int);
+ostream& operator<<(ostream&, vector<double>);
+ostream& operator<<(ostream&, vector<vector<double>>);
+
+
+int main() {
+    vector<vector<double>> A{ {2, 4, 3, 5}, {-4, -7, -5, -8}, {6, 8, 2, 9}, {4, 9, -2, 14}};
+    cout << "Matrix A:\n" << A << endl;
+    inverseByLU(A, 4, 4);
+    
+}
+
+ostream& operator<<(ostream& os, vector<double> matrix) {
+    for (double d : matrix) {
+        os << d << " ";
+    }
+    os << "\n";
+    return os;
+}
+ostream& operator<<(ostream& os, vector<vector<double>> matrix) {
+    for (vector<double> row : matrix) {
+        os << row;
+    }
+    return os;
+}
+
+void inverseByLU(vector<vector<double>> A, int Rows, int Cols) {
+
+    vector<vector<double>> L( Rows, vector<double>(Cols, 0) );
+    vector<vector<double>> U( Rows, vector<double>(Cols, 0) );
+
+
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            if (i == j) {
+                L[i][i] = 1;
+                U[i][j] = 0;
+            }
+            else {
+                L[i][j] = 0;
+                U[i][j] = 0;
+            }
+        }
+    }
+
+    
+    for (int i = 0; i < Rows; i++) {
+        
+        for (int j = i + 1; j < Cols; j++) {
+            double pivot = A[j][i] / A[i][i];
+            L[j][i] = pivot;
+            for (int k = 0; k < Cols; k++) {
+                A[j][k] -= A[i][k] * pivot;
+            }
+
+        }
+    }
+
+    for (int i = 0; i < Rows; i++) {
+        U[i] = A[i];
+    }
+    
+
+    cout << "Matrix L:\n";
+    cout << L << endl;
+    cout << "Matrix U:\n";
+    cout << U << endl;
+    
+
+}
+
+void inverseMatrix(vector<vector<double>> triangleMatrix, int Rows, int Cols) {
+    
+    if (triangleMatrix[0][1] != 0) {
+        for (int i = 0; i < Rows; i++) {
+            double pivot = triangleMatrix[i][i];
+            triangleMatrix[i][i] = 1;
+            for (int j = i + 1; j < Cols; j++) {
+                triangleMatrix[i][j] /= pivot;
+            }
+        }
+
+    
+    }
+};
---
+++ Term1/Algoritms/Lab2/CyclicShift/CyclicShift/CyclicShift.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool strIsShift(string str1, string str2) {
+
+	int curLen = 0;
+
+	//Якщо довжина 1 рядка не така сама як у 2, то вона не може бути циклічним зсувом
+	if (str1.length() != str2.length()) {
+		return false;
+	}
+
+	//Якщо продублювати 1 рядок то ми отримуємол рядок з усіма можливими зсувами
+	string str3 = str1 + str1;
+
+	for (int i = 0; i < str3.length(); i++) {
+		if (str3[i] == str2[curLen]) {
+			//Збільшуємо значення лічильника якщо збігаються відповідні сиволи
+			++curLen;
+			continue;
+		} else if (curLen == str2.length()) {
+			return true;
+		} else if (i > str1.length() + 1 && curLen ==0) {
+		//Оптимізація: якщо і більше ніж довжина 1 рядка + 1 ->, то довжина 2 рядка буде більше за кількість символів, що залишились
+			return false;
+		}
+		//Якщо символи не співпадають обнуляємо лічильник
+			curLen = 0;
+	}
+
+	//Якщо цикл не повернув true => рядок 2 не є циклічним зсувом першого
+	return false;
+} 
+
+
+int main() {
+	string s1, s2;
+	
+	cout << "\n\tFirst string: ";
+	cin >> s1;
+	cout << "\n\tString to check: ";
+	cin >> s2;
+	cout << "\n\t" << s2 + (strIsShift(s1, s2) ?  " is shifted " : " is not shifted ");
+}
+
